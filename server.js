25a3aaf232cd810f9b97eb7a15677bc30ca9a492
pradeep100ran <103431083+pradeep100ran@users.noreply.github.com>
2022-12-27 23:35:06 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,22 +1,54 @@
-'use strict';
 var r=0;
-const express = require('express');
-const { Server } = require('ws');
 
-const PORT = process.env.PORT || 3000;
-const INDEX = '/index.html';
+const WebSocket = require('ws');
 
-const server = express()
-  .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
-  .listen(PORT, () => console.log(`Listening on ${PORT}`));
+const PORT = 5000;
 
-const wss = new Server({ server });
+const wsServer = new WebSocket.Server({
 
-wss.on('connection', (ws) => {
-  console.log('Client connected');
-  ws.on('close', () => console.log('Client disconnected'));
+    port: PORT
+
 });
 
+wsServer.on('connection', function (socket) {
+
+    // Some feedback on the console
+
+    console.log("A client just connected");
+
+    // Attach some behavior to the incoming socket
+
+    socket.on('message', function (msg) {
+
+        console.log("Received message from client: "  + msg);
+
+        // socket.send("Take this back: " + msg);
+
+        // Broadcast that message to all connected clients
+
+        wsServer.clients.forEach(function (client) {
+
+            //client.send(msg*msg);
+
+          
+
+          
+
+      //client.send(Math.floor(Math.random() * 1000));
+
+          
+
+          
+
+          
+
+          
+
+         });
+
+    });
+
+});
 
 function makeid(length) {
 
@@ -36,24 +68,78 @@ function makeid(length) {
 
 }
 
-
-
+//console.log(makeid(5));
 
 setInterval(() => {
-  wss.clients.forEach((client) => {
 
-function generate_token(length){
-    //edit the token allowed characters
-    var a = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890".split("");
-    var b = [];  
-    for (var i=0; i<length; i++) {
-        var j = (Math.random() * (a.length-1)).toFixed(0);
-        b[i] = a[j];
-    }
-    return b.join("");
-}
-r++;  
-client.send('Request No = ' + r + ' Time = ' + aa + ' Id = ' + makeid(5));
-    //client.send(generate_token(64));
-  });
-}, 1);
+wsServer.clients.forEach((client) => {
+
+    
+
+//let d = new Date();
+
+  
+
+  
+
+  
+
+    let dt = new Date();
+
+ let   d = new Date(dt.getTime() + 330 * 60 * 1000);
+
+      
+
+      
+
+    r++;  
+
+      
+
+let aa=(${d.getHours()}:${d.getMinutes()}:${d.getSeconds()});
+
+    client.send('Request No = ' + r + ' Time = ' + aa + ' Id = ' + makeid(5));
+
+  //var nf=18450+ Math.floor(Math.random() * 10);
+
+  //var nf =
+
+   //   (Math.round(nf * 100) / 100).toFixed(2);
+
+  //client.send(tt);
+
+  
+
+  
+
+ // var bnf=44800+Math.floor(Math.random()*40);
+
+ // var bnf=(Math.round(bnf*100)/100).tofixed(2);
+
+  
+
+  
+
+ // var feed = '{'
+
+     //  +'"nifty":"'+nf+'",'
+
+     //  +'"banknifty":"'+nf+'",'
+
+    //   +'"time":"'+d+
+
+      // '"}';
+
+  
+
+ // client.send(feed);
+
+  
+
+  
+
+});
+
+},1);
+
+console.log( (new Date()) + " Server is listening on port " + PORT);
